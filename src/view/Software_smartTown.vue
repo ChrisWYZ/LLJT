90aacb7fd0211f7e2f35b56d26c9450537171ec7
ChrisWYZ <15808930230@163.com>
2020-09-08 10:22:26 +0800

    '初始化版本1.0.0'

---
+++ src/view/Software_smartTown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="smartTown">
+        噜噜噜噜噜
+    </div>
+</template>
+<script>
+export default {
+    name: 'smartTown',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
