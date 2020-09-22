6aba13ae31494043164f93d8471f2e608b2b9916
wuyu999 <457902061@qq.com>
2020-09-22 14:32:35 +0800

    922wy

--- src/view/CompanyIntroduction.vue
+++ src/view/CompanyIntroduction.vue
@@ -7,17 +7,54 @@
                     <img class="img-responsive center-block" src="@/assets/img/LLdamen.jpg">
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-6">
-                    <h3>&nbsp;&nbsp;龙利集团有限责任公司</h3>
-                    <p class=".text-justify">&nbsp;&nbsp;&nbsp;&nbsp;海南自贸区龙利电子商务有限公司成立于2019年，注册在海南省澄迈县老城镇高新技术产业示范区海南生态软件园，实际办公地点位于海口市海垦路119号，公司主营业务为进出口再生纸浆、纸制品、国内废纸贸易及其他再生资源业务。
-在中央大力发展推动海南自贸港的背景下，我公司享受海南自贸港进出口零关税、企业所得税减半15%的优惠政策，预计到2021年，我公司纳税达1个亿。公司办公面积500多平方米，为我公司法人名下自有商铺。
-  我公司拥有专业的管理团队、专业的海关报关清关团队，公司的企业愿景是成为中国领先的再生资源服务商。2020年公司注册并开始运营海南贸易网，致力打造成为海内外进出口贸易信息化电子商务服务平台。</p>
+                    <h3><strong>&nbsp;&nbsp;龙利集团有限责任公司</strong></h3>
+                    <hr />
+                    <p class=".text-justify">&nbsp;&nbsp;&nbsp;&nbsp;海南龙利跨境电商集团有限公司，简称“龙利集团”,目前全国已经拥有近10个关联公司，2020年在中央宣布成立海南自贸港的政策下，龙利集团加快了整合步伐，以海南自贸区龙利电子商务有限责任公司为母公司，整合海南龙利国际贸易公司、海南隆利源国际贸易有限公司、亳州龙利再生资源、亳州龙翔木业、亳州隆利丰再生资源、亳州隆利源再生资源、黑龙江龙利再生资源等10个再生资源公司，公司业务扩展到境外的美国、欧洲、东南亚国家，境内海南、广东、广西、福建、上海、重庆、河南、安徽、黑龙江等省份。
+龙利集团注册地址海南省海口市海垦路119号九九华府C栋2001，集团公司主营业务为互联网批发：互联网+再生资源、互联网+财税服务、互联网+国际贸易。</p>
                     <p class=".text-justify">&nbsp;&nbsp;&nbsp;&nbsp;欢迎想来海南享受自贸港优惠政策及寻找商机的朋友联系我们！我们提供从工商注册、公司办公、记账报税、在线交易、代办进出口资质、报关清关等一站式服务！</p>
                     <!-- <p class=".text-justify">&nbsp;&nbsp;&nbsp;&nbsp;33333333333333333333333333333333333333333333.</p> -->
                 </div>
             </div>
         </div>
         <!-- 图片宣传 -->
+         <div class="laydiv">
+            <!-- <div class="container"> -->
+            <!-- <div class="CompangIntroduction-container row"> -->
+        <div class="swiper-slide customer-block" 
+        >
         
+            <div class="customer-logo col-xs-12 col-sm-4 col-md-4">
+                <img v-image-preview class="center-block" src="@/assets/img/hainanll1.jpg"  width="250px" height="250px" alt="picture" >
+            </div>
+            <div class="customer-logo col-xs-12 col-sm-4 col-md-4">
+                <img v-image-preview class="center-block" src="@/assets/img/hainanll2.jpg" width="250px" height="250px" alt="picture" >
+            </div>
+            <div class="customer-logo col-xs-12 col-sm-4 col-md-4">
+                <img v-image-preview class="center-block" src="@/assets/img/hainanll3.jpg" width="250px" height="250px" alt="picture" >
+            </div>
+            
+        </div>
+        <!-- 联系我们 -->
+    <div id="contactUs" class="container-fuild text-center">
+      <div class="container contactUs-container wow slideInUp">
+        <h1>诚实守信&nbsp;知行合一</h1>
+        <h3>用双手开拓进取，用能力创造未来。</h3>
+        <h3>言必信、行必果！</h3>
+        <h3>人人讲诚信、事事做诚信！</h3>
+        <router-link to="/ContactUS"><button 
+          class="btn btn-default btn-sm"
+          onmouseleave="this.style.borderColor='#ffffff'; this.style.backgroundColor='#ffffff'; this.style.color='#3f3f3f';"
+          onmouseenter="this.style.backgroundColor='transparent'; this.style.borderColor='#ffffff'; this.style.color='#ffffff';"
+        >联系我们</button></router-link>
+        <div class="contactUs-contactWay">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+    </div>
+
     </div>
     
 </template>
@@ -27,17 +64,7 @@ export default {
     name: 'CompanyIntroduction',
     data(){
         return{
-            List:[
-                {
-                    img: require("@/assets/img/hainanll1.jpg")
-                },
-                {
-                    img: require("@/assets/img/hainanll2.jpg")
-                },
-                {
-                    img: require("@/assets/img/hainanll3.jpg")
-                }
-            ]
+            
         }
     },
     mounted(){
@@ -76,7 +103,64 @@ export default {
     padding: 10px 0;
     color: #808080;
 }
+    img {
+    width: 40%;
+    height:40%;
+    padding: 2px;
+    background: white;
+    float: right;
+    border: 0.5px solid #efefef;
+    border-radius: 10%;
+    }
+    .item > img {
+        width: 260px;
+        height: 165px;
+        position: relative;
+        right: 16%;
+    }
+    .laydiv{
+        width: 100%;
+    }
 
+    #tu{
+        margin:4%;
+        width: 25.3%;
+        height: 150px;
+        float: left;
+        position: relative;
+        border: 0.5px solid #efefef;
+        border-radius: 15%;
+
+    }
+
+}
+/* 联系我们 */
+#contactUs {
+  color: #fff;
+  height: 400px;
+  background: url("../assets/img/kouhao.jpg") 0 0 no-repeat;
+  background-size: 100% 100%;
+  transition: all ease 0.6s;
+  padding: 80px;
+}
+/* 口号+按钮 */
+#contactUs .contactUs-container {
+  padding: 0px;
+} 
+/* 按钮 */
+ #contactUs .contactUs-container button {
+  width: 300px;
+  height: 50px;
+  margin-top:20px;
+} 
+ #contactUs .contactUs-container .contactUs-contactWay span {
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  margin: 30px;
+} 
+.customer-block {
+height: 300px;
 }
 </style>
 
