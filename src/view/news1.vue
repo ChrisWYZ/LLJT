5f39d390f072f3d5ca2ad88631ec97df29bb47e6
wuyu999 <457902061@qq.com>
2020-10-13 17:11:45 +0800

    1013wy1

--- src/view/news1.vue
+++ src/view/news1.vue
@@ -1,40 +1,147 @@
 <template>
   <div id="News">    
     <div class="title text-center">
-      <h4> 2020（第四届）中国品牌博鳌峰会
-        <small>/News headlines</small>
+      <h4> 热烈庆祝龙利集团在2020年第四届中国品牌博鳌峰会斩获殊荣
+        <!-- <small>/News headlines</small> -->
       </h4>
     </div> 
     <div class="container">
       <p>
-        中国品牌博鳌峰会是由中国商报社和中国企业网联合举办，其核心使命是推动中国品牌快速成长，积极宣传展示优质品牌企业，不断寻找和挖掘优秀品牌故事，
-        为壮大中国优秀品牌阵营、培育高质量发展新动能贡献力量。
-      </p>  
-      <p>
-        2020年9月26日至27日在海南博鳌隆重举办，峰会设有博鳌品牌人物颁奖典礼、中国品牌论坛、中国自主品牌500强发布会、品牌新闻发布厅、
-        中国自主品牌展览会等多个主题活动，并邀请有关部委领导、知名品牌专家、品牌领袖和各地区优秀企业家代表等各界嘉宾出席，凝聚品牌发展智慧，
-        研判品牌建设形势，加强行业间交流，促进企业间合作，为经济增长助力加油。
-      </p> 
-      <p>
-        在这中国品牌博鳌峰会中我们公司非常荣幸的获“中国行业十大影响力品牌”奖、“中国自主品牌500强”奖;公司宋军董事长获选“中国新时代十大品牌人物”。
-        非常的感谢大领导们对我们公司的认可和肯定。
-        </p>          
+        9月26-27日，由中国商报社、中国企业网联合主办的“第四届中国品牌博鳌会”在海南博鳌顺利举行，我公司受邀参评，龙利集团董事长兼总经理宋军、总经理助理张红、副总经理卢莹珊、副总经理崔园代表公司出席系列活动。此 次峰会中龙利集团荣获“中国行业十大影响力品牌”奖、“中国自主品牌500强”奖、公司宋军董事长获选“中国新时代十大品牌人物”奖。
+      </p>     
       </div>
       <!-- 宣传图片添加 -->
       <div class="container">
           <div class="picture"> 
             <div class="row">
-              <div class=" col-sm-6" id="demo1">
-                  <img src="../assets/img/boaofenghui1.jpg" alt="">             
-              </div>
-              <div class=" col-sm-6"  id="demo2">
-                  <img src="../assets/img/boaofenghui2.jpg" alt=""> 
-              </div>            
+                  <img src="../assets/img/boao01.jpg" alt="">             
             </div>
           </div> 
-        </div>                         
-</div>
-  
+        </div>
+      <!-- 标题 -->
+      <div class="biaoti" style="text-align:center">
+      <p>
+       <strong> 董事长宋军（左一）代表公司领奖</strong>
+      </p>
+      </div>
+      <!-- 宣传图片添加 -->
+      <div class="container">
+          <div class="picture"> 
+            <div class="row">
+                  <img src="../assets/img/boao02.jpg" alt="">             
+            </div>
+          </div> 
+        </div>
+      <!-- 标题 -->
+      <div class="biaoti" style="text-align:center">
+      <p>
+       <strong>董事长宋军（右三）获“中国新时代十大品牌人物”奖</strong>
+      </p>
+      </div>  
+      <div class="container">
+      <p>
+        此次峰会邀请了国家领导人、有关部委领导、知名专家学者、品牌领袖和全国各地优秀企业家代表等嘉宾出席，并开展中国自主品牌500强发布会、主题论坛、品牌新闻发布厅，中国自主品牌展览会、博鳌品牌人物颁奖典礼、全渠道资源对接会、投融资对接会等主题活动聚焦品牌强国战略，探讨品牌发展挑战和机遇。
+      </p>     
+      </div>
+      <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+                  <img src="../assets/img/boao03.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>此次峰会与会领导、嘉宾</strong></p>
+          </div>
+          <!-- wenben -->
+          <div class="container">
+      <p>
+        海南龙利跨境电商集团有限公司，简称“龙利集团”,目前全国已经拥有近 10个关联公司，2020年在中央宣布成立海南自贸港的政策下，龙利集团加快了整合步伐，以海南自贸区龙利电子商务有限责任公司为母公司，整合海南龙利国际贸易公司、海南隆利源国际贸易有限公司、亳州龙利再生资源回收有限公司、亳州龙翔木业有限公司共同出资成立了龙利集团，公司业务扩展到境外的美国、 欧洲、东南亚国家，境内海南、广东、广西、福建、上海、重庆、河南、安 徽、黑龙江等省份。 集团公司主营业务为互联网批发：互联网+再生资源、互联网+国际贸易、互联网+财税服务。
+      </p>     
+      </div>
+      <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao04.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>董事长宋军参会</strong></p>
+          </div>
+          <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao05.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>董事长宋军参加行业论坛（右一）</strong></p>
+          </div>
+          <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao06.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>董事长宋军在第四届中国品牌博鳌峰会分享交流</strong></p>
+          </div>
+          <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao07.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>副总经理卢莹珊（左）、总经理助理张红（右）参会</strong></p>
+          </div>
+          <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao09.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>副总经理崔园参会</strong></p>
+          </div>
+          <!-- 图片 -->
+        <div class="container">
+          <div class="picture"> 
+            <div class="row">
+              <!-- <div class=" col-sm-6" id="demo1"> -->
+                  <img src="../assets/img/boao10.jpg" alt="">             
+              </div>
+              </div>
+          </div>
+          <!-- 图片标题 -->
+          <div class="biaoti" style="text-align:center">
+            <p><strong>海南龙利集团代表团队合影</strong></p>
+          </div>
+
+      
+  </div>
 </template>
 <style >
  #News>.title>h4 {
@@ -47,12 +154,12 @@
     width: 70%;  
     padding: 2px;
 }
-#News>.container>p:first-letter {
+/* #News>.container>p:first-letter {
    font-style:italic;
     font-size: 30px;
     float: left;
     text-shadow: 3px 3px 3px greenyellow;    
-}
+} */
 #News>.container>p {
    font-family:"华文陆帅","Arial","Angsana New";
     font-size: 20px;
